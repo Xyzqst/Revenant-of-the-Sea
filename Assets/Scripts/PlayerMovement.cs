ad4d32f2dab5ee9b2d69e8798d2e4a854a6a1f3c
Xyzqst <xyzmation@gmail.com>
2026-07-01 17:24:59 +0800

    Basic Player movement
    Finished the script for the player movement , the capsule is able to move around the scene vertically and horizontally.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+//Variables
+
+public float speed = 5f;
+
+private Rigidbody2D rb;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(x,y) * speed;
+
+        rb.linearVelocity = movement;
+        Debug.Log(x);
+        Debug.Log(y);
     }
 }
