86dae85525603986c299cb5241c30838a4e38b7f
Xyzqst <xyzmation@gmail.com>
2026-07-06 04:00:29 +0800

    Attacking base on mouse position
    Added code that allows the player to attack base on mouse cursor direction instead of moevement direction. This is still in progress but the player will attack based on the direction through the debug.log

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,11 +1,12 @@
-using NUnit.Framework.Internal;
+
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
 
     public Animator anim;
-    public Test = 
+    public Rigidbody2D rb;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,13 +21,44 @@ public class PlayerCombat : MonoBehaviour
         {
             Debug.Log("CLICKED");
             attack();
-             Debug.Log(mouseposition);
+             
         }
     }
 
     //player attack method
     void attack()
     {
-        Vector3 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+     
+    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    Vector2 direction = mousePosition - (Vector2)transform.position;
+
+   
+    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+    {
+       
+        if (direction.x > 0)
+        {
+            Debug.Log("Attack Right!");
+        }
+
+        else
+        {
+            Debug.Log("Attack Left!");
+        }
+
     }
+    else
+    {
+       
+        if (direction.y > 0)
+        {
+            Debug.Log("Attack Up!");
+        }
+        else
+        {
+            Debug.Log("Attack Down!");
+        }
+    }
+    }
+    
 }
