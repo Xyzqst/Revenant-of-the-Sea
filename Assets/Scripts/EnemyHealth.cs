d7b75716ceb8eb04af7de939802fe991cf13300b
Xyzqst <xyzmation@gmail.com>
2026-08-11 13:42:38 +0800

    Added enemy mini health bar
    Added a slider for the enemy where the player can visually see the enemy hp go down

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,26 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class EnemyHealth : MonoBehaviour
 {
 
     public int currentHealth;
     public int maxHealth = 5;
+   
     
+     public Slider enemyHealthBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         currentHealth = maxHealth;
+
+        enemyHealthBar.gameObject.SetActive(currentHealth < maxHealth);
+        enemyHealthBar.value = currentHealth;
+        enemyHealthBar.maxValue = maxHealth;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyHealthBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 1f);
+
+
     }
 
     public void damageHealth(int amount)
     {
+        
         currentHealth -= amount;
+        enemyHealthBar.gameObject.SetActive(true);
+        enemyHealthBar.value = currentHealth;
 
         if(currentHealth <= 0)
         {
